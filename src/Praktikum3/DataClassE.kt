077b4237dcfbd01a3ad9cab8c168826c7a467a31
SD-24 <fitridestika22@gmail.com>
2024-09-24 09:40:34 +0700

    Konsep OOP

---
+++ src/Praktikum3/DataClassE.kt
@@ -0,0 +1,9 @@
+package Praktikum3
+
+fun main() {
+    val meri = Student("Meri", 20)
+    // Destructuring Declaration
+    val(name, age) = meri
+    println("Name = $name")
+    println("Age = $age")
+}
